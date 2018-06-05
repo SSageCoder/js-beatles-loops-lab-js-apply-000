4b22bde9cc9db846b9dc9e77eb4f8571489a3464
ssingle2002@gmail.com <ssingle2002@gmail.com>
2018-06-05 03:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 
-const johnLennonFacts() ["He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"];
-
 function theBeatlesPlay(muscians, instruments) {
     var allMuscians = [];
     for (var i=0; i < muscians.length; i++) {
         allMuscians.push(muscians[i] + " plays " + instruments[i]);
     }
     return allMuscians;
+}
+
+function johnLennonFacts() {
+    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+    var newFacts = [];
+    var i = 0;
+    while (i < facts.length) {
+        newFacts.push(facts[i] +"!!!");
+        i++;
+    }
+    return newFacts;
 }
