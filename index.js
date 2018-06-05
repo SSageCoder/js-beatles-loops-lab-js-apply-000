b1a53c436df71e844131dc9ec2eae8805fb3ea5c
ssingle2002@gmail.com <ssingle2002@gmail.com>
2018-06-05 03:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 
-const facts = [  "He was the last Beatle to learn to drive",
+const johnLennonFacts() ["He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
+  "He hated the sound of his own voice"];
 
 function theBeatlesPlay(muscians, instruments) {
     var allMuscians = [];
