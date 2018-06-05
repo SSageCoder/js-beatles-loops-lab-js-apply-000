f5b6e3d821936852289cf0dbfef482f48af881e5
ssingle2002@gmail.com <ssingle2002@gmail.com>
2018-06-05 04:02:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ function johnLennonFacts() {
     }
     return newFacts;
 }
+
+function iLoveTheBeatles(num) {
+    var love = [];
+    do {
+        love.push("I love the Beatles!");
+        num++;
+    } while (num < 15);
+    
+    return love;
+}
